e7ed1976870a95cd062c80ad0056e6f2cc7fed06
eduardo516 <76666752+eduardo516@users.noreply.github.com>
2025-03-06 14:03:59 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,21 @@
+  /* 
+Bautista de Jesus Héctor Eduardo
+Ejercicio 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int a=3;
+    int b=2;
+    
+    if(a>b){
+        printf("a es mayor \n");
+    }
+    
+    
+    return 0;
+    
+}
